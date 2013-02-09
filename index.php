07f42562ea581090f6628dd6a7401ef4189d8a0a
Rasmus Kjellberg <rk@youngmedia.se>
2013-02-09 23:36:50 +0100

    single file download

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ include_once ( 'mp3scraper.class.php' );
 // Fetch a new URL.
 $mp3 = new mp3scraper('http://www.radiorecord.ru/radio/top100/detail.php?station=4901');
 
-// Set string to match when searching for mp3s.
-$mp3->regexp 	= "audio.radiorecord.ru(.*)\.mp3";
-
-// Set download directory.
+// Set download directory. (default: downloads/)
 $mp3->directory('radiorecord');
 
 // Do it..
--- mp3scraper.class.php
+++ mp3scraper.class.php
@@ -1,52 +1,63 @@
 <?php
-	class mp3scraper
+class mp3scraper
+{
+	var $url;
+	var $urlcontent;
+	var $directory;
+	var $downloadlinks = array();
+
+	var $regexp = 'href=\"http:\/\/(.*)\.mp3';
+	var $rootdir = 'downloads/';
+
+	public function __construct( $url = false )
 	{
-		var $url;
-		var $urlcontent;
-		var $regexp;
-		var $directory;
-		var $downloadlinks = array();
-		var $rootdir = 'downloads/';
-
-		public function __construct( $url = false )
-		{
-			$this->directory = $directory;
+		$this->directory = $directory;
 
 
-			if (!is_dir('downloads'))
-				mkdir('downloads');
+		if (!is_dir('downloads'))
+			mkdir('downloads');
 
-			if ( empty( $url ) )
-				die("Usage: mp3scrape(\$directory = 'random', \$url = false) \n");
+		if ( empty( $url ) )
+			die("Usage: mp3scrape(\$directory = 'random', \$url = false) \n");
 
-			else 
-				$this->url = $url; 
-		}
-
-		public function getlist()
-		{
-		  	$this->urlcontent = @file_get_contents( $this->url ) or die("Could not access URL:". $this->url );
-		  	echo "Fetching mp3s.. \n";
-
-			if ( preg_match_all ( "/$this->regexp/siU", $this->urlcontent, $downloadlinks, PREG_SET_ORDER ) ) 
-			
-			return $downloadlinks;
-		}
-
-		public function directory ( $dir )
-		{
-			$this->directory = $dir;
-			if (!is_dir($this->rootdir . $this->directory))
-				mkdir($this->rootdir . $this->directory);
-		}
-
-		public function download ( )
-		{
-			
-
-			echo "aa \n";
-		}
+		else 
+			$this->url = $url; 
 	}
+
+	public function getlist()
+	{
+	  	$this->urlcontent = @file_get_contents( $this->url ) or die("Could not access URL:". $this->url );
+	  	echo "Fetching mp3s.. \n";
+
+		if ( preg_match_all ( "/$this->regexp/siU", $this->urlcontent, $output, PREG_SET_ORDER ) ) 
+		
+		foreach ( $output as $tmpurlarray ):
+
+			// Remove href=" and whitespaces.
+			$tmpurl = str_replace(' ', '%20', $tmpurlarray[0] );
+			$tmpurl = str_replace('href="', '', $tmpurl );
+
+			$downloadlinks[] = $tmpurl;
+		endforeach;
+
+		return $downloadlinks;
+	}
+
+	public function directory ( $dir )
+	{
+		$this->directory = $dir;
+		if (!is_dir($this->rootdir . $this->directory))
+			mkdir($this->rootdir . $this->directory);
+	}
+
+	public function download ( )
+	{
+		$downloads = $this->getlist();	
+		foreach ($downloads as $download):
+			echo "$download\n";
+		endforeach;
+	}
+}
 
 
 
